fd6bf0b9ed5ab778e35ee5404af3949c729ac55c
Tamas Kis <tamas.a.kis@outlook.com>
2024-11-27 19:07:33 -0800

    Re-writing Scalar trait as an extension of num_traits::Float (#16)

--- src/scalar.rs
+++ src/scalar.rs
@@ -1,6 +1,6 @@
-use num_traits::Zero;
+use num_traits::Float;
 use std::fmt::Debug;
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign};
 
 /// Scalar type.
 ///
@@ -22,33 +22,38 @@ use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 /// [`nalgebra::Complex`] does not satisfy the the [`Scalar`] trait because it does not implement
 /// the [`PartialOrd`] trait.
 pub trait Scalar:
-    // Arithmetic operators with itself.
-    Add<Self, Output = Self>
-    + Sub<Self, Output = Self>
-    + Mul<Self, Output = Self>
-    + Div<Self, Output = Self>
+    // Float trait from num-traits, providing:
+    //  --> Copy
+    //  --> PartialEq
+    //  --> PartialOrd
+    //  --> Add<Self, Output = Self>
+    //  --> Sub<Self, Output = Self>
+    //  --> Mul<Self, Output = Self>
+    //  --> Div<Self, Output = Self>
+    //  --> Rem<Self, Output = Self>
+    //  --> Neg<Output = Self>
+    //  --> Zero
+    //  --> One
+    //  --> Standard mathematical methods (i.e. most methods implement for f64s).
+    Float
     // Arithmetic-assignment operators with itself.
     + AddAssign<Self>
     + SubAssign<Self>
     + MulAssign<Self>
     + DivAssign<Self>
+    + RemAssign<Self>
     // Arithmetic operators with f64.
     + Add<f64, Output = Self>
     + Sub<f64, Output = Self>
     + Mul<f64, Output = Self>
     + Div<f64, Output = Self>
+    + Rem<f64, Output = Self>
     // Arithmetic-assignment operators with f64.
     + AddAssign<f64>
     + SubAssign<f64>
     + MulAssign<f64>
     + DivAssign<f64>
-    // Needed for construction.
-    + Zero
-    // Other "standard" traits for primitive numeric types.
-    + Copy
-    + Clone
-    + PartialEq
-    + PartialOrd
+    // Debug printing.
     + Debug
     // Type must be defined at compile time.
     + 'static
@@ -56,27 +61,22 @@ pub trait Scalar:
 }
 
 impl<T> Scalar for T where
-    T: Add<Self, Output = Self>
-        + Sub<Self, Output = Self>
-        + Mul<Self, Output = Self>
-        + Div<Self, Output = Self>
+    T: Float
         + AddAssign<Self>
         + SubAssign<Self>
         + MulAssign<Self>
         + DivAssign<Self>
+        + RemAssign<Self>
         + Add<f64, Output = Self>
         + Sub<f64, Output = Self>
         + Mul<f64, Output = Self>
         + Div<f64, Output = Self>
+        + Rem<f64, Output = Self>
         + AddAssign<f64>
         + SubAssign<f64>
         + MulAssign<f64>
         + DivAssign<f64>
-        + Zero
-        + Copy
-        + Clone
-        + PartialEq
-        + PartialOrd
+        + RemAssign<f64>
         + Debug
         + 'static
 {
